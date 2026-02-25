e32680b61986e2920e1fbf5ec9b1117517975e5f
Raghu Bandaru <raghubandaru45@gmail.com>
2026-02-25 18:02:48 +0530

    Add server component

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,54 +1,81 @@
-"use client";
+import { getFiles } from "@/services/fileService";
 
-import { useEffect, useState } from "react";
-
-type FileType = {
-  _id: string;
-
-  url: string;
-
-  filename: string;
-
-  createdAt: string;
-};
-
-export default function FilesPage() {
-  const [files, setFiles] = useState<FileType[]>([]);
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("/api/files")
-      .then((res) => res.json())
-
-      .then((data) => {
-        setFiles(data);
-
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
+export default async function FilesList() {
+  const files = await getFiles();
 
   return (
-    <div>
-      <h2>Uploaded Files</h2>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, 200px)",
-          gap: "20px",
-        }}
-      >
-        {files.map((file) => (
-          <div key={file._id}>
-            <img src={file.url} width={200} />
-
-            <p>{file.filename}</p>
-          </div>
-        ))}
-      </div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, 200px)",
+        gap: "20px",
+      }}
+    >
+      {files.map((file) => {
+        return (
+          <img
+            key={file._id.toString()}
+            src={file.url}
+            alt={file.filename}
+            width={200}
+          />
+        );
+      })}
     </div>
   );
 }
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+
+// type FileType = {
+//   _id: string;
+
+//   url: string;
+
+//   filename: string;
+
+//   createdAt: string;
+// };
+
+// export default function FilesPage() {
+//   const [files, setFiles] = useState<FileType[]>([]);
+
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetch("/api/files")
+//       .then((res) => res.json())
+
+//       .then((data) => {
+//         setFiles(data);
+
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) return <p>Loading...</p>;
+
+//   return (
+//     <div>
+//       <h2>Uploaded Files</h2>
+
+//       <div
+//         style={{
+//           display: "grid",
+//           gridTemplateColumns: "repeat(auto-fill, 200px)",
+//           gap: "20px",
+//         }}
+//       >
+//         {files.map((file) => (
+//           <div key={file._id}>
+//             <img src={file.url} width={200} />
+
+//             <p>{file.filename}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,19 +2,15 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URI!;
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-
+  const client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
 
-clientPromise = global._mongoClientPromise;
+const clientPromise: Promise<MongoClient> = global._mongoClientPromise!;
 
 export default clientPromise;
